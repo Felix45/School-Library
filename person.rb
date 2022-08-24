b9cd41cc37b42707ea5063ab4cb862b49b641dc5
Felix Ouma <fatonoh@gmail.com>
2022-08-24 23:27:59 +0300

    Add class association between book, rental and person class

--- person.rb
+++ person.rb
@@ -6,10 +6,11 @@ class Person < Nameable
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @rentals = []
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   private
@@ -26,5 +27,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
   attr_accessor :date
+  attr_reader :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    @book = book
+
+    @book.add_rental(self)
+    @person.add_rental(self)
   end
 end
