6a5f76734cf718486afaefa38c293903718372ce
Felix Ouma <fatonoh@gmail.com>
2022-08-29 16:17:40 +0300

    Extract all listing methods to List class

---
+++ list.rb
@@ -0,0 +1,28 @@
+require_relative 'helpers'
+
+class List
+  def List.list_people(people)
+    puts
+    people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+    end
+  end
+
+  def List.list_books(books)
+    puts
+    books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+
+  def List.list_rentals(people)
+    puts
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+    person = Finder.find(people, person_id)
+    puts 'Rentals:'
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date} Book: #{rental.book.title} by #{rental.book.author}"
+    end
+  end
+end
