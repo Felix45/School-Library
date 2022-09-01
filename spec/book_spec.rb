7195d80a3f2edbadb8eec3f7ec39d5b264797872
Felix Ouma <fatonoh@gmail.com>
2022-09-01 10:55:32 +0300

    Add test for the Book class Add test to test creation of book object Add test to test renting out book to a person

---
+++ spec/book_spec.rb
@@ -0,0 +1,22 @@
+require '../book'
+require '../student'
+
+describe Book do
+  before :each do
+    @book = Book.new('Think Big', 'Ben Carson')
+  end
+  context 'book with correct details' do
+    it 'book should have title: Think Big, author: Ben Carson' do
+      expect(@book.title).to eq('Think Big')
+      expect(@book.author).to eq('Ben Carson')
+      expect(@book.rentals).to eq []
+    end
+  end
+
+  context "Book rented out, " do
+    it 'adding one rental to book should have length of one' do
+      @book.add_rental('2022/08/30', Student.new(20, nil, 'Felix Ouma'))
+      expect(@book.rentals.length).to eq(1)
+    end
+  end
+end
